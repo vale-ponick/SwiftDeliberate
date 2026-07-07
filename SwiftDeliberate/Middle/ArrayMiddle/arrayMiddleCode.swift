ab0db28c516a0f1bb6ed76c1fa7a04e6ceb7f99b
valeponick <vale.ponick@gmail.com>
2026-07-07 18:10:40 +0700

    "📐🚕💨 docs: add Arrays Middle theory - lazy, reduce(into:), performance, nested arrays, ArraySlice"

---
+++ SwiftDeliberate/Middle/ArrayMiddle/arrayMiddleCode.swift
@@ -0,0 +1,8 @@
+//
+//  arrayMiddleCode.swift
+//  SwiftDeliberate
+//
+//  Created by Валерия Пономарева on 07.07.2026.
+//
+
+import Foundation
---
+++ SwiftDeliberate/Middle/ArrayMiddle/arrayMiddleTheory.swift
@@ -0,0 +1,8 @@
+//
+//  arrayMiddleTheory.swift
+//  SwiftDeliberate
+//
+//  Created by Валерия Пономарева on 07.07.2026.
+//
+
+import Foundation
