87e9c8c5b9d6238bf26c653f67181dcdee693366
valeponick <vale.ponick@gmail.com>
2026-07-18 15:57:09 +0700

    "🏆 feat: add safe CinemaHall subscript with row and seat validation"

---
+++ SwiftDeliberate/Tasks/SubscriptTasks/subscriptTasksReflect.swift
@@ -0,0 +1,12 @@
+//
+//  subscriptTasksReflect.swift
+//  SwiftDeliberate
+//
+//  Created by Валерия Пономарева on 18.07.2026.
+//
+
+import Foundation
+/**
+ 🏆 ЗОЛОТОЕ ПРАВИЛО
+ Сабскрипт может принимать несколько параметров. Всегда проверяй каждый индекс отдельно через guard.
+ */
