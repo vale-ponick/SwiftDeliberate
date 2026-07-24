245e3de169358ee05e2033ee57cb55ddf987b7b8
valeponick <vale.ponick@gmail.com>
2026-07-24 23:40:47 +0700

    "📚 docs: add comprehensive cheatsheet on Control Flow and Loops in Swift"

--- SwiftDeliberate/Easy/ControlFlaw/controlFlowCode.swift
+++ SwiftDeliberate/Easy/ControlFlaw/controlFlowCode.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct ControlFlow {
+    static func run() {
+        print("▶️ Запуск: Control Flow (Easy)")
+              
+              // MARK: - 1️⃣: Task
+        
+        for index in 1...3 {
+            print("Итерация номер \(index)")
+            /* Итерация номер 1
+            Итерация номер 2
+            Итерация номер 3 */
+        }
+    }
+}
+
